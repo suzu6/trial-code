d45c1fe3c70a9243aa9275b147f47e40f7f0c7ad
suzu6 <y.suzumura0604@gmail.com>
2021-03-18 00:25:18 +0900

    Add rust module

---
+++ rust/hello_cargo/restaurant/src/lib.rs
@@ -0,0 +1,50 @@
+mod front_of_house {
+    pub mod hosting {
+        pub fn add_to_waitlist() {}
+
+        fn seat_at_table() {}
+    }
+
+    mod serving {
+        fn take_order() {}
+
+        fn serve_order() {}
+
+        fn take_payment() {}
+    }
+}
+
+
+pub fn eat_at_restaurant() {
+    // Absolute path
+    // 絶対パス
+    crate::front_of_house::hosting::add_to_waitlist();
+
+    // Relative path
+    // 相対パス
+    front_of_house::hosting::add_to_waitlist();
+}
+
+use crate::front_of_house::hosting;
+
+pub fn take_at_restaurant() {
+    // 関数は親のパスまで、関数がローカルで定義されていないことを明らかにできます
+    hosting::add_to_waitlist();
+    hosting::add_to_waitlist();
+    hosting::add_to_waitlist();
+}
+
+// 構造体やenumその他の要素をuseで持ち込むときは、フルパスを書くのが慣例的
+use std::collections::HashMap;
+
+fn main() {
+    let mut map = HashMap::new();
+    map.insert(1, 2);
+}
+
+
+use std::cmp::Ordering;
+use std::io;
+
+
+use std::{cmp::Ordering, io};
