46adbf1ae677db40db4eb676dea03db3612b721f
suzu6 <y.suzumura0604@gmail.com>
2021-03-18 15:26:16 +0900

    Add gcd and min, max

--- rust/algorithm/src/lib.rs
+++ rust/algorithm/src/lib.rs
@@ -3,35 +3,47 @@ extern crate num_traits;
 
 #[cfg(test)]
 mod tests {
-    #[test]
-    fn test_sum() {
-        assert_eq!(
-            crate::math::sum_one_to_n(10),
-            55
-        );
-    }
+
+    mod test_sum{
+        #[test]
+        fn test_sum() {
+            assert_eq!(
+                crate::math::sum_one_to_n(10),
+                55
+            );
+        }
+        
+        #[test]
+        fn test_sum_one() {
+            assert_eq!(
+                crate::math::sum_one_to_n(1),
+                1
+            );
+        }
     
-    #[test]
-    fn test_sum_one() {
-        assert_eq!(
-            crate::math::sum_one_to_n(1),
-            1
-        );
+        #[test]
+        #[should_panic]
+        fn test_sum_minus() {
+            let minus: i32 = -1;
+            // 負の値はpanicを返す
+            crate::math::sum_one_to_n(minus);
+        }
+    
+        #[test]
+        #[should_panic]
+        fn test_sum_zero() {
+            let zero: i32 = 0;
+            // 0はpanicを返す
+            crate::math::sum_one_to_n(zero);
+        }
     }
 
-    #[test]
-    #[should_panic]
-    fn test_sum_minus() {
-        let minus: i32 = -1;
-        // 負の値はpanicを返す
-        crate::math::sum_one_to_n(minus);
-    }
-
-    #[test]
-    #[should_panic]
-    fn test_sum_zero() {
-        let zero: i32 = 0;
-        // 0はpanicを返す
-        crate::math::sum_one_to_n(zero);
+    mod test_gcd{
+        #[test]
+        fn test_gcd() {
+            assert_eq!(12, crate::math::gcd(12, 24));
+            assert_eq!(3, crate::math::gcd(12, 21));
+            assert_eq!(1, crate::math::gcd(12, 1));
+        }
     }
 }
--- rust/algorithm/src/math.rs
+++ rust/algorithm/src/math.rs
@@ -1,14 +1,49 @@
 use num_traits::PrimInt;
 
 /// 1からnまでの総和を算出する
-pub fn sum_one_to_n<T: PrimInt>(n: T) -> u32 {
-  let n: u32 = n.to_u32().unwrap();
+pub fn sum_one_to_n<T: PrimInt>(n: T) -> i32 {
+  let n: i32 = n.to_i32().unwrap();
   if n <= 0 {
     panic!("n is not natural number.");
   }
   if n == 1 {
     1
-  }else{
+  } else {
     n + sum_one_to_n(n - 1)
+  }
+}
+
+/// 最大公約数
+pub fn gcd(a: i32, b: i32) -> i32 {
+  if a < 0 || b < 0 {
+    panic!("a or b is not natural number.");
+  }
+  gcd_recrusive(max(a, b), min(a, b))
+}
+/// ユークリッドの互除法
+fn gcd_recrusive(a: i32, b: i32) -> i32 {
+  let c = a % b;
+  if c == 0 {
+    b
+  } else {
+    gcd_recrusive(min(a, b), c)
+  }
+}
+
+/// 小さい方を返す
+pub fn min(a: i32, b: i32) -> i32 {
+  if a < b {
+    a
+  } else {
+    b
+  }
+}
+
+/// 大きい方を返す
+pub fn max(a: i32, b: i32) -> i32 {
+  if a > b {
+    a
+  } else {
+    b
   }
 }
