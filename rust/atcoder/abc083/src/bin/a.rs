e4d60eba7a56a037b609467482c801aedf4a9781
suzu6 <y.suzumura0604@gmail.com>
2021-03-24 11:07:09 +0900

    Add abc83

--- rust/atcoder/abc083/src/bin/a.rs
+++ rust/atcoder/abc083/src/bin/a.rs
@@ -1,3 +1,15 @@
 fn main() {
-    todo!();
+    proconio::input! {
+        a: i32,
+        b: i32,
+        c: i32,
+        d: i32,
+    }
+    if a+b > c+d {
+        println!("Left");
+    }else if a+b == c+d {
+        println!("Balanced");
+    }else {
+        println!("Right");
+    }
 }
--- rust/atcoder/abc083/src/bin/c.rs
+++ rust/atcoder/abc083/src/bin/c.rs
@@ -1,3 +1,13 @@
 fn main() {
-    todo!();
+    proconio::input! {
+        x: u64,
+        y: u64,
+    }
+    let mut count = 0;
+    let mut a = x; 
+    while a <= y {
+        count += 1;
+        a *= 2;
+    }
+    println!("{}", count);
 }
