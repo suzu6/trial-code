cd47764f48fee508cbf689a1e05b62eb2fa94e4b
suzu6 <y.suzumura0604@gmail.com>
2021-04-14 00:38:13 +0900

    Update Mandelbrot集合の画像描画

--- rust/algorithms/mandelbrot/src/main.rs
+++ rust/algorithms/mandelbrot/src/main.rs
@@ -1,17 +1,60 @@
+extern crate image;
 extern crate num;
+use image::png::PngEncoder;
+use image::ColorType;
 use num::Complex;
+use std::fs::File;
 use std::str::FromStr;
+use std::io::Write;
 
 fn main() {
-    let z = Complex { re: 1.0, im: 0.5 };
-    let limit: u32 = 100;
-    let result = complex_square_loop(z, limit);
-    println!("{:?}", result);
-    println!("Hello, world!");
+    let args: Vec<String> = std::env::args().collect();
+
+    if args.len() != 5 {
+        writeln!(std::io::stderr(),
+                 "Usage: mandelbrot FILE PIXELS UPPERLEFT LOWERRIGHT")
+            .unwrap();
+        writeln!(std::io::stderr(),
+                 "Example: {} mandel.png 1000x750 -1.20,0.35 -1,0.20",
+                args[0])
+            .unwrap();
+        std::process::exit(1);
+    }
+
+    let bounds = parse_pair(&args[2], 'x')
+        .expect("error parsing image dimensions");
+    let upper_left = parse_complex(&args[3])
+        .expect("error parsing upper left corner point");
+    let lower_right = parse_complex(&args[4])
+        .expect("error parsing lower right corner point");
+
+    let mut pixels = vec![0; bounds.0 * bounds.1];
+
+    render(&mut pixels, bounds, upper_left, lower_right);
+
+    write_image(&args[1], &pixels, bounds)
+        .expect("error writing PNG file");
+}
+
+/// Write the buffer `pixels`, whose dimensions are given by
+/// `bounds`, to the file named `filename`.
+fn write_image(
+    filename: &str,
+    pixels: &[u8],
+    bounds: (usize, usize),
+) -> Result<(), image::ImageError> {
+    let output = File::create(filename)?;
+
+    let encoder = PngEncoder::new(output);
+    let result = encoder.encode(&pixels, bounds.0 as u32, bounds.1 as u32, ColorType::L8);
+    match result {
+        Ok(()) => {return Ok(()); },
+        Err(e) => {return Err(e); }
+    }
 }
 
 #[allow(dead_code)]
-fn complex_square_loop(c: Complex<f64>, limit: u32) -> Option<u32> {
+fn escape_time(c: Complex<f64>, limit: u32) -> Option<u32> {
     let mut z = Complex { re: 0.0, im: 0.0 };
     for i in 0..limit {
         z = z * z + c;
@@ -21,6 +64,25 @@ fn complex_square_loop(c: Complex<f64>, limit: u32) -> Option<u32> {
     }
 
     None
+}
+
+fn render(
+    pixels: &mut [u8],
+    bounds: (usize, usize),
+    upper_left: Complex<f64>,
+    lower_right: Complex<f64>,
+) {
+    assert!(pixels.len() == bounds.0 * bounds.1);
+
+    for row in 0..bounds.1 {
+        for colmun in 0..bounds.0 {
+            let point = pixel_to_point(bounds, (row, colmun), upper_left, lower_right);
+            pixels[row * bounds.0 + colmun] = match escape_time(point, 255) {
+                None => 0,
+                Some(count) => 255 - count as u8,
+            };
+        }
+    }
 }
 
 fn pixel_to_point(
