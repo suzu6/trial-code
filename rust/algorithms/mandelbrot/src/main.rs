164d6a791f33273ceb275a89c72ff0fef5ab91ba
suzu6 <y.suzumura0604@gmail.com>
2021-04-13 23:41:25 +0900

    Add pixel to point function

--- rust/algorithms/mandelbrot/src/main.rs
+++ rust/algorithms/mandelbrot/src/main.rs
@@ -23,11 +23,27 @@ fn complex_square_loop(c: Complex<f64>, limit: u32) -> Option<u32> {
     None
 }
 
+fn pixel_to_point(
+    bounds: (usize, usize),
+    pixel: (usize, usize),
+    upper_left: Complex<f64>,
+    lower_right: Complex<f64>,
+) -> Complex<f64> {
+    let (width, height) = (
+        lower_right.re - upper_left.re,
+        upper_left.im - lower_right.im,
+    );
+    Complex {
+        re: upper_left.re + pixel.0 as f64 * width / bounds.0 as f64,
+        im: upper_left.im - pixel.1 as f64 * height / bounds.1 as f64,
+    }
+}
+
 // Parse a pair of floating-point numbers separated by a comma as a complex number.
-fn parse_complex(s: &str) ->  Option<Complex<f64>> {
+fn parse_complex(s: &str) -> Option<Complex<f64>> {
     match parse_pair(s, ',') {
-        Some((re, im)) => Some(Complex{ re, im }),
-        None => None
+        Some((re, im)) => Some(Complex { re, im }),
+        None => None,
     }
 }
 
@@ -42,11 +58,28 @@ fn parse_pair<T: FromStr>(s: &str, separator: char) -> Option<(T, T)> {
 }
 
 #[test]
-fn test_parse_complex() {
-    assert_eq!(parse_complex("1.25,-0.00625"), 
-               Some(Complex {re: 1.25, im: -0.00625}));
-    assert_eq!(parse_complex(", -0.00625"), None);
+fn test_pixel_to_point() {
+    assert_eq!(
+        pixel_to_point(
+            (100, 100),
+            (25, 75),
+            Complex { re: -1.0, im: 1.0 },
+            Complex { re: 1.0, im: -1.0 }
+        ),
+        Complex { re: -0.5, im: -0.5 }
+    );
+}
 
+#[test]
+fn test_parse_complex() {
+    assert_eq!(
+        parse_complex("1.25,-0.00625"),
+        Some(Complex {
+            re: 1.25,
+            im: -0.00625
+        })
+    );
+    assert_eq!(parse_complex(", -0.00625"), None);
 }
 
 #[test]
