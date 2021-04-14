75a81b066f09e1d22a8390b96951471b486e6a96
suzu6 <y.suzumura0604@gmail.com>
2021-04-14 14:59:49 +0900

    Fix 列と行が反対だったことと、カラムのつづりを修正

--- rust/algorithms/mandelbrot/src/main.rs
+++ rust/algorithms/mandelbrot/src/main.rs
@@ -110,9 +110,9 @@ fn render(
     assert!(pixels.len() == bounds.0 * bounds.1);
 
     for row in 0..bounds.1 {
-        for colmun in 0..bounds.0 {
-            let point = pixel_to_point(bounds, (row, colmun), upper_left, lower_right);
-            pixels[row * bounds.0 + colmun] = match escape_time(point, 255) {
+        for column in 0..bounds.0 {
+            let point = pixel_to_point(bounds, (column, row), upper_left, lower_right);
+            pixels[row * bounds.0 + column] = match escape_time(point, 255) {
                 None => 0,
                 Some(count) => 255 - count as u8,
             };
