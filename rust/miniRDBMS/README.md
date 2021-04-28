71f0ae884a2b7b81f6c551122ff4e0eb498aec26
suzu6 <y.suzumura0604@gmail.com>
2021-04-28 18:17:35 +0900

    Update disk.rs and success test

--- rust/miniRDBMS/README.md
+++ rust/miniRDBMS/README.md
@@ -8,3 +8,29 @@ https://github.com/KOBA789/relly
 は本人が動画を出しているので、こちらを見てもらうと内容についてわかりやすいです。
 これ見てから本を買いました。
 https://www.youtube.com/watch?v=eWDkIz9BN0A
+
+## 実装してみて
+
+書籍のサンプルを写経して実行はできない。
+GuHubのコードとCargo.tomlのクレートの依存関係を書いておかないとエラーになる。
+
+ファイルをコピペしてきて、処理ごとにコメントを書くのが良いかも。
+
+ひとファイルずつ書いてテストするには`main.rs`に`mod disk;`と書いてから`cargo test`を実行する必要がある。
+
+```rust:main.rs
+mod disk;
+```
+上述がないとテスト出来ない。（`cargo test disk`もだめ）
+```bash
+> cargo test
+    Finished test [unoptimized + debuginfo] target(s) in 0.02s
+     Running target\debug\deps\mini_rdbms-84b4747aa01fcfe2.exe
+
+running 0 tests
+
+test result: ok. 0 passed; 0 failed; 0 ignored; 0 measured; 0 filtered out; finished in 0.00s
+```
+
+
+とりあえず、
--- rust/miniRDBMS/src/disk.rs
+++ rust/miniRDBMS/src/disk.rs
@@ -1,17 +1,67 @@
+use std::io::{self, prelude::*, SeekFrom};
+use std::path::Path;
 /// ディスクマネージャ
 /// https://github.com/KOBA789/relly/blob/wdb/src/disk.rs
 ///
 /// io::Resultはio系のエラーで中身が成功したときの返り値
 /// Selfは自分の型と()はviod
 /// &mut selfがないメソッドはスタティックメソッド
-/// 
-/// // p78ではdata_file_pathだが、p79からのコードはheap_file_pathになっている（文中はdata_file_path）。
+///
+/// p78ではdata_file_pathだが、p79からのコードはheap_file_pathになっている（文中はdata_file_path）。
+use std::{
+  convert::TryInto,
+  fs::{File, OpenOptions},
+};
+
+use zerocopy::{AsBytes, FromBytes};
+
+pub const PAGE_SIZE: usize = 4096;
+
+/// ページIDを表す型
+/// 足し算など無意味な演算をエラーにしておく
+#[derive(Debug, Clone, Copy, Eq, PartialEq, Hash, FromBytes, AsBytes)]
+#[repr(C)]
+pub struct PageId(pub u64);
+impl PageId {
+  pub const INVALID_PAGE_ID: PageId = PageId(u64::MAX);
+
+  pub fn valid(self) -> Option<PageId> {
+      if self == Self::INVALID_PAGE_ID {
+          None
+      } else {
+          Some(self)
+      }
+  }
+
+  pub fn to_u64(self) -> u64 {
+      self.0
+  }
+}
 
 pub struct DiskManager {
   // ヒープファイルのファイルディスクリプタ
   heap_file: File,
   // 採番するページIDを決めるカウンタ
   next_page_id: u64,
+}
+
+impl Default for PageId {
+  fn default() -> Self {
+    Self::INVALID_PAGE_ID
+  }
+}
+
+impl From<Option<PageId>> for PageId {
+  fn from(page_id: Option<PageId>) -> Self {
+    page_id.unwrap_or_default()
+  }
+}
+
+impl From<&[u8]> for PageId {
+  fn from(bytes: &[u8]) -> Self {
+    let arr = bytes.try_into().unwrap();
+    PageId(u64::from_ne_bytes(arr))
+  }
 }
 
 impl DiskManager {
@@ -22,34 +72,34 @@ impl DiskManager {
     let next_page_id = heap_file_size / PAGE_SIZE as u64;
     Ok(Self {
       heap_file,
-      nextpage_id,
+      next_page_id,
     })
   }
 
   // ファイルパスを指定して開く
-  // pub fn open(data_file_path: impl AsRef<Path>) -> io::Result<Self> { 
+  // pub fn open(data_file_path: impl AsRef<Path>) -> io::Result<Self> {
   pub fn open(heap_file_path: impl AsRef<Path>) -> io::Result<Self> {
     let heap_file = OpenOptions::new()
       .read(true)
       .write(true)
-      .creat(true)
+      .create(true)
       .open(heap_file_path)?; // I/Oエラーが起きるかもしれいない。エラーの場合は早期リターン
     Self::new(heap_file)
   }
 
   // 新しいページIDを採番する
   pub fn allocate_page(&mut self) -> PageId {
-    let page_id  =self.next_page_id;
+    let page_id = self.next_page_id;
     self.next_page_id += 1; // 採番する度にインクリメントする
     PageId(page_id)
   }
 
   // ページのデータを読みだす
-  pub fn read_page_data(&mut self, page_id: PageId, data: &[u8]) -> io::Result<()> {
+  pub fn read_page_data(&mut self, page_id: PageId, data: &mut [u8]) -> io::Result<()> {
     // オフセットを計算
     let offset = PAGE_SIZE as u64 * page_id.to_u64();
     // ページ先頭へシーク
-    selt.heap_file.seek(SeekFrom::Start(offset))?;
+    self.heap_file.seek(SeekFrom::Start(offset))?;
     // データを書きこむ
     self.heap_file.read_exact(data)
   }
@@ -59,12 +109,51 @@ impl DiskManager {
     // オフセットを計算
     let offset = PAGE_SIZE as u64 * page_id.to_u64();
     // ページ先頭へシーク
-    selt.heap_file.seek(SeekFrom::Start(offset))?;
+    self.heap_file.seek(SeekFrom::Start(offset))?;
     // データを書きこむ
     self.heap_file.write_all(data)
   }
+
+  pub fn sync(&mut self) -> io::Result<()> {
+    self.heap_file.flush()?;
+    self.heap_file.sync_all()
+  }
 }
 
-/// ページIDを表す型
-/// 足し算など無意味な演算をエラーにしておく
-pub struct  PageId(pub u64);
+#[cfg(test)]
+mod tests {
+  use super::*;
+  use tempfile::NamedTempFile;
+
+  #[test]
+  fn test_disk() {
+    let (data_file, data_file_path) = NamedTempFile::new().unwrap().into_parts();
+    let mut disk = DiskManager::new(data_file).unwrap();
+    let mut hello = Vec::with_capacity(PAGE_SIZE);
+    hello.extend_from_slice(b"hello");
+    hello.resize(PAGE_SIZE, 0);
+    let hello_page_id = disk.allocate_page();
+    disk.write_page_data(hello_page_id, &hello).unwrap();
+    let mut world = Vec::with_capacity(PAGE_SIZE);
+    world.extend_from_slice(b"world");
+    world.resize(PAGE_SIZE, 0);
+    let world_page_id = disk.allocate_page();
+    disk.write_page_data(world_page_id, &world).unwrap();
+    drop(disk);
+    let mut disk2 = DiskManager::open(&data_file_path).unwrap();
+    let mut buf = vec![0; PAGE_SIZE];
+    disk2.read_page_data(hello_page_id, &mut buf).unwrap();
+    assert_eq!(hello, buf);
+    disk2.read_page_data(world_page_id, &mut buf).unwrap();
+    assert_eq!(world, buf);
+  }
+
+  // #[test]
+  // fn test_positive() {
+  //   assert!(true);
+  // }
+  // #[test]
+  // fn test_negative() {
+  //   assert!(false);
+  // }
+}
